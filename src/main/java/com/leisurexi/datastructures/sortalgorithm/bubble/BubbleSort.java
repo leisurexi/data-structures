ec82c947b2c6e1bade21e15672e14ecc5d548db4
leisurexi <leisurexi@126.com>
2019-12-08 16:23:00 +0800

    ==> leisurexi

--- src/main/java/com/leisurexi/datastructures/sortalgorithm/bubble/BubbleSort.java
+++ src/main/java/com/leisurexi/datastructures/sortalgorithm/bubble/BubbleSort.java
@@ -102,12 +102,57 @@ public class BubbleSort {
         }
     }
 
+    /**
+     * 鸡尾酒排序
+     * 代码外层的大循环控制着所有排序回合，大循环内包含2个小循环，从第1个小循环从
+     * 左向右比较并交换元素，第2个小循环从右向左比较并交换元素。
+     *
+     * @param array
+     */
+    public static void cocktailSort(int[] array) {
+        int temp = 0;
+        for (int i = 0; i < array.length / 2; i++) {
+            //有序标记，每一轮的初始值都是true
+            boolean isSorted = true;
+            //奇数轮，从左向右比较和交换
+            for (int j = i; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    //有元素交换，所以不是有序的，标记为false
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+            //在偶数轮之前，将isSorted重新标记为true
+            isSorted = true;
+            //偶数轮，从右向左比较和交换
+            for (int j = array.length - i - 1; j > i; j--) {
+                if (array[j] < array[j - 1]) {
+                    temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                    //有元素交换，所以不是有序的，标记为false
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
 //        int[] array = {5, 8, 6, 3, 9, 2, 1, 7};
 //        sort_1(array);
 //        sort_2(array);
-        int[] array = {3, 4, 2, 1, 5, 6, 7, 8};
-        sort_3(array);
+//        int[] array = {3, 4, 2, 1, 5, 6, 7, 8};
+//        sort_3(array);
+        int[] array = {2, 3, 4, 5, 6, 7, 8, 1};
+        cocktailSort(array);
         log.info(Arrays.toString(array));
     }
 
