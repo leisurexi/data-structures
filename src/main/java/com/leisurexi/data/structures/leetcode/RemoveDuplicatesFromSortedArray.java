e508da63d7f6f2fb13ac61ccda72cba9f30e7b55
leisurexi <leisurexi@126.com>
2020-03-16 23:32:48 +0800

    ==> leisurexi

--- src/main/java/com/leisurexi/data/structures/leetcode/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/leisurexi/data/structures/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -77,9 +77,33 @@ public class RemoveDuplicatesFromSortedArray {
         return index + 1;
     }
 
+    /**
+     * 方法：双指针法
+     * 算法
+     * 数组完成排序后，我们可以放置两个指针 ii 和 jj，其中 ii 是慢指针，而 jj 是快指针。
+     * 只要 nums[i] = nums[j]nums[i]=nums[j]，我们就增加 jj 以跳过重复项。
+     * 当我们遇到 nums[j] \neq nums[i]nums[j]=nums[i] 时，跳过重复项的运行已经结束，
+     * 因此我们必须把它（nums[j]nums[j]）的值复制到 nums[i + 1]nums[i+1]。然后递增 ii，
+     * 接着我们将再次重复相同的过程，直到 jj 到达数组的末尾为止。
+     */
+    public static int removeDuplicates_2(int[] nums) {
+        if (nums.length < 1) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 2};
-        log.info(String.valueOf(removeDuplicates(nums)));
+//        log.info(String.valueOf(removeDuplicates(nums)));
+        log.info(String.valueOf(removeDuplicates_2(nums)));
         log.info(Arrays.toString(nums));
     }
 
