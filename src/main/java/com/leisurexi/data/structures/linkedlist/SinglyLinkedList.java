16877c8ef970157ac410583f193d9d811df384e1
leisurexi <leisurexi@gmail.com>
2020-10-14 14:18:10 +0800

    ==> 使用链表判断字符串是否是回文字符串

--- src/main/java/com/leisurexi/data/structures/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/leisurexi/data/structures/linkedlist/SinglyLinkedList.java
@@ -12,8 +12,8 @@ public class SinglyLinkedList {
 
     private Node head;
 
-    public void addFirst(String value) {
-        for (char c : value.toCharArray()) {
+    public SinglyLinkedList(String str) {
+        for (char c : str.toCharArray()) {
             Node newNode = new Node(c, head);
             head = newNode;
         }
@@ -27,39 +27,58 @@ public class SinglyLinkedList {
      * 1 -> 2 -> 3
      */
     public boolean isPalindrome() {
+        // 快指针
         Node fastPointer = this.head;
+        // 慢指针
         Node slowPointer = this.head;
-        while (fastPointer.next != null && slowPointer.next.next != null) {
+        while (fastPointer != null && fastPointer.next != null) {
             fastPointer = fastPointer.next.next;
             slowPointer = slowPointer.next;
         }
 
-        if (fastPointer != null) {
-            // 奇数个节点
-            Node leftLink = null;
-            Node rightLink = null;
-
-
-
-        } else {
-            // 偶数个节点
-
-
+        Node reverseNode = reverse(slowPointer);
+        Node headNode = this.head;
+        boolean isPalin = true;
+        while (headNode != null && reverseNode != null) {
+            if (headNode.e.equals(reverseNode.e)) {
+                headNode = headNode.next;
+                reverseNode = reverseNode.next;
+            } else {
+                isPalin = false;
+                break;
+            }
         }
-
-
-        return false;
+        return isPalin;
     }
 
     /**
      * 翻转链表
      * <p>
      * 1 -> 2 -> 3
-     * 2
+     * next: 2->3
+     * head: 1->null
+     * reverseHead: 1->null
+     * head: 2->3
      */
-    public Node reverseLinkedList(Node head) {
+    private Node reverse(Node head) {
+        Node reverseHead = null;
+        while (head != null) {
+            Node next = head.next;
+            head.next = reverseHead;
+            reverseHead = head;
+            head = next;
+        }
+        return reverseHead;
+    }
 
-        return null;
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node node = this.head;
+        do {
+            sb.append(node.e + "\n");
+        } while ((node = node.next) != null);
+        return sb.toString();
     }
 
 
@@ -74,9 +93,12 @@ public class SinglyLinkedList {
     }
 
     public static void main(String[] args) {
-        LinkedList linkedList = new LinkedList();
-
-
+        String[] strs = {"aba", "abb", "abba", "qwerewq"};
+        for (String str : strs) {
+            SinglyLinkedList linkedList = new SinglyLinkedList(str);
+            boolean isPalindrome = linkedList.isPalindrome();
+            System.out.println(isPalindrome);
+        }
     }
 
 }
